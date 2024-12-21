167aa40cf0b5c4b018b1e76ae903801ddc12010f
Gantulga-kh <gantulga.khuyag@gmail.com>
2024-12-21 20:06:53 +0800

    v0.0.119

--- pi.sh
+++ pi.sh
@@ -112,6 +112,14 @@ pitunnel --port=5900 --host=$2 --persist --name=$1-TABLET
 
 echo "Installing Node / Redis / PM2 [...]"
 
+# sudo apt-get update
+# sudo apt-get install -y ca-certificates curl gnupg
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+NODE_MAJOR=16 
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
 sudo apt-get -y install nodejs
 sudo apt-get -y install npm
 sudo apt-get -y install nginx
