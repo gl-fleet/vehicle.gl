4137549e9c35e04b724c22637d336a73b1da03dd
gantulgak <gantulgak@riotinto.com>
2025-04-28 14:53:27 +0800

    v0.0.134

--- core/data/src/index.ts
+++ core/data/src/index.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from 'sequelize'
 import { Host, Connection, rSlave } from 'unet'
-import { decodeENV, Safe, Shell, env, log } from 'utils'
+import { decodeENV, Safe, Shell, env, log, Delay } from 'utils'
 
 import { Event } from './event'
 import { Chunk } from './chunk'
@@ -62,41 +62,45 @@ Safe(async () => {
 
     await cf.sequelize.sync({ force: false, alter: true })
 
-    Safe(() => {
+    Delay(() => {
 
-        if (process.pid && Shell.which('renice')) {
+        Safe(() => {
 
-            Shell.exec(`echo '${mode === 'development' ? 'tulgaew' : 'umine'}' | sudo -S renice -n -19 -p ${process.pid}`)
-            log.success(`Renice: Process re-niced ${process.pid}`)
+            if (process.pid && Shell.which('renice')) {
 
-        } else log.error(`Renice: Couldn't start`)
+                Shell.exec(`echo '${mode === 'development' ? 'tulgaew' : 'umine'}' | sudo -S renice -n -19 -p ${process.pid}`)
+                log.success(`Renice: Process re-niced ${process.pid}`)
 
-    }, 'Renice')
+            } else log.error(`Renice: Couldn't start`)
 
-    Safe(() => {
+        }, 'Renice')
 
-        const wifi = Shell.exec(`echo 'umine' | sudo -S ip neigh show dev wlan0`, { silent: true }).stdout
-        const wifi_list = wifi.split('\n')
+        Safe(() => {
 
-        for (const x of wifi_list) {
+            const wifi = Shell.exec(`echo 'umine' | sudo -S ip neigh show dev wlan0`, { silent: true }).stdout
+            const wifi_list = wifi.split('\n')
 
-            /**
-             * Valid "x": 10.42.0.55 lladdr b0:60:88:82:4d:3a REACHABLE
-             */
+            for (const x of wifi_list) {
 
-            const str = x.split(' ')
-            const dots = (str[0].split('.') || []).length
+                /**
+                 * Valid "x": 10.42.0.55 lladdr b0:60:88:82:4d:3a REACHABLE
+                 */
 
-            if (dots === 4) {
+                const str = x.split(' ')
+                const dots = (str[0].split('.') || []).length
 
-                const command = `echo '${mode === 'development' ? 'tulgaew' : 'umine'}' | sudo -S iptables -A FORWARD -s ${str[0]} -j DROP`
-                Shell.exec(command, { silent: true }).stdout
-                log.success(`IP-Table / executed -> ${command}`)
+                if (dots === 4) {
+
+                    const command = `echo '${mode === 'development' ? 'tulgaew' : 'umine'}' | sudo -S iptables -A FORWARD -s ${str[0]} -j DROP`
+                    Shell.exec(command, { silent: true }).stdout
+                    log.success(`IP-Table / executed -> ${command}`)
+
+                }
 
             }
 
-        }
+        }, 'IP-Table')
 
-    }, 'IP-Table')
+    }, 30 * 1000)
 
 })
